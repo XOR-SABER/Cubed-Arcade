dbf2b0d56aa8d1d221a719cd5cff118001d0e39c
ProTime00 <108126773+ProTime00@users.noreply.github.com>
2024-05-08 22:19:09 -0700

    removed discord from color picker because of some trouble with build

--- CubedPrime/Assets/Scripts/MainMenu/ColorPicker.cs
+++ CubedPrime/Assets/Scripts/MainMenu/ColorPicker.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Discord;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class ColorPicker : MonoBehaviour
 {
     public float currentHue;
